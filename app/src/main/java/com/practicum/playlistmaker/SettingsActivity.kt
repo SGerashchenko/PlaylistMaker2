843a49968ccc6bda1ce9dc1940ecd6b9931b5b9f
Сергей <luis_figo_7@mail.ru>
2024-12-13 22:12:08 +0300

    First commit

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+val backMenu: ImageView = findViewById(R.id.back_menu)
+        backMenu.setOnClickListener {
+    val displayIntent = Intent(this, MainActivity::class.java)
+    startActivity(displayIntent)}}}
+
+
