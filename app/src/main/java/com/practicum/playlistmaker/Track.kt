afad9abf286dddcc4668f89a5a40e4e5dc83177e
Сергей <luis_figo_7@mail.ru>
2025-01-05 15:16:32 +0300

    BACK

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,8 +1,11 @@
 package com.practicum.playlistmaker
 
- class Track(
+import com.google.gson.annotations.SerializedName
+
+class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    @SerializedName("trackTimeMillis")
+    val trackTime: Long,
     val artworkUrl100: String
 )
